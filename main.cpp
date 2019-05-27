e7dd64927771621f666e0a2244131c70fe0aee48
BaumFX <15325915+BaumFX@users.noreply.github.com>
2019-05-27 22:31:42 +0200

    Update main.cpp
    added comments

--- main.cpp
+++ main.cpp
@@ -3,10 +3,16 @@
 #include <algorithm>
 #include <ctime>
 #include <chrono>
+//string array to hold character sets
+const std::string chars[] = { "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "!\"#$%&'()*+,-./:;<=>?@[]^_{|}~", "0123456789" };
+//string to hold similar characters (to remove)
+const std::string hahayes = "O0l1I5S2Z";
 
-const std::string CHR[] = { "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "!\"#$%&'()*+,-./:;<=>?@[]^_{|}~", "0123456789" };
-const std::string UNS = "O0l1I5S2Z";
-
+/**
+	function to check if a given string reference is an integer
+	@param reference to string
+  	@return boolean if the string is an integer or not
+*/
 bool is_int(std::string const& n) noexcept
 {
 	if (std::isdigit(n[0]) || (n.size() > 1 && (n[0] == '-' || n[0] == '+')))
@@ -20,14 +26,19 @@ bool is_int(std::string const& n) noexcept
 	return false;
 }
 
+/**
+	function to generate a password
+	@param integer for length
+  	@return string with password
+*/
 std::string generate_password(int len) {
 	std::string pw;
 	char t;
 	for (int x = 0; x < len; x += 4) {
 		for (int y = x; y < x + 4 && y < len; y++) {
 			do {
-				t = CHR[y % 4].at(rand() % CHR[y % 4].size());
-			} while (UNS.find(t) != UNS.npos);
+				t = chars[y % 4].at(rand() % chars[y % 4].size());
+			} while (hahayes.find(t) != hahayes.npos);
 			pw.append(1, t);
 		}
 	}
@@ -50,6 +61,7 @@ int main()
 	int pwamount = std::stoi(pwamount_string);
 	int pwlength = std::stoi(pwlength_string);
 
+	//measure execution time of password generation
 	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
 	for (int c = 0; c < pwamount; c++) {
